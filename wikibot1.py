16669c779e7b455ad6a6d46dba5c8bf36ff9c6f4
GrzegorzDjango <48175719+GrzegorzDjango@users.noreply.github.com>
2023-06-21 11:30:39 +0000

    adding wikibot code

--- wikibot1.py
+++ wikibot1.py
@@ -1,7 +1,7 @@
 import wikipedia
 
 def scrape(name="Microsoft", length=1):
-    result = wikipedia.summary(name sentences=length)
+    result = wikipedia.summary(name, sentences=length)
     return result
 
 print(scrape("Wikipedia"))

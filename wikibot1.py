2c3ef23dc4cfcd0c67395f7d578b6f99e4d3ac8b
GrzegorzDjango <48175719+GrzegorzDjango@users.noreply.github.com>
2023-06-21 11:36:37 +0000

    Wiki

--- wikibot1.py
+++ wikibot1.py
@@ -1,7 +1,16 @@
+import click
 import wikipedia
 
+
+@click.command()
+@click.option('--name', default='Microsoft', help='Wikipedia word')
 def scrape(name="Microsoft", length=1):
+    """
+    just scraping
+    """
     result = wikipedia.summary(name, sentences=length)
+    click.echo(click.style(f"{result}", fg="red"))
     return result
 
-print(scrape("Wikipedia"))
+if __name__=='__main__':
+    scrape()
